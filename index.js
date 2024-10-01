fa8e3dbe4465740b75098d2946cd3f6d8a760d45
victor <mwakiretivictor@gmail.com>
2024-10-01 12:53:14 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetNumber){
+  // Write your code here! 
+if(feetNumber <= 400){
+return "This one is on me!"
+}
+ else if  (feetNumber >= 400 && feetNumber <= 2000){
+  return "That will be twenty bucks."
+}
+ else if (feetNumber > 2000 && feetNumber <= 2500 ){
+ return "I will gladly take your thirty bucks."
+}
+ else {
+  return "No can do."
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cities){
   // Write your code here!
+  return cities === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip){
+  case "generous" : 
+    return "Thank you so much."
+  case "not as generous" : 
+    return "Thank you."
+  default :  
+    return "Bye."
+}
 }
